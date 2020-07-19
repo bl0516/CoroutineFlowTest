8acc2d2bd43ce51c756f3662be30b00540ce4f68
bl0516 <60408140+bl0516@users.noreply.github.com>
2020-07-19 09:20:21 -0700

    Initial commit

---
+++ app/src/main/java/com/hoc/flowmvi/App.kt
@@ -0,0 +1,32 @@
+package com.hoc.flowmvi
+
+import android.app.Application
+import com.hoc.flowmvi.koin.dataModule
+import com.hoc.flowmvi.koin.domainModule
+import com.hoc.flowmvi.koin.viewModelModule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+@Suppress("unused")
+@FlowPreview
+@ExperimentalCoroutinesApi
+class App : Application() {
+  override fun onCreate() {
+    super.onCreate()
+
+    startKoin {
+      androidContext(this@App)
+
+      androidLogger()
+
+      modules(
+          dataModule,
+          domainModule,
+          viewModelModule
+      )
+    }
+  }
+}
---
+++ app/src/main/java/com/hoc/flowmvi/ui/add/AddContract.kt
@@ -0,0 +1,62 @@
+package com.hoc.flowmvi.ui.add
+
+import com.hoc.flowmvi.domain.entity.User
+import kotlinx.coroutines.flow.Flow
+
+interface AddContract {
+  interface View {
+    fun intents(): Flow<ViewIntent>
+  }
+
+  enum class ValidationError {
+    INVALID_EMAIL_ADDRESS,
+    TOO_SHORT_FIRST_NAME,
+    TOO_SHORT_LAST_NAME
+  }
+
+  data class ViewState(
+      val errors: Set<ValidationError>,
+      val isLoading: Boolean
+  ) {
+    companion object {
+      fun initial() = ViewState(
+          errors = emptySet(),
+          isLoading = false
+      )
+    }
+  }
+
+  sealed class ViewIntent {
+    data class EmailChanged(val email: String?) : ViewIntent()
+    data class FirstNameChanged(val firstName: String?) : ViewIntent()
+    data class LastNameChanged(val lastName: String?) : ViewIntent()
+    object Submit : ViewIntent()
+  }
+
+  sealed class PartialStateChange {
+    abstract fun reduce(viewState: ViewState): ViewState
+
+    data class ErrorsChanged(val errors: Set<ValidationError>) : PartialStateChange() {
+      override fun reduce(viewState: ViewState) = viewState.copy(errors = errors)
+    }
+
+    sealed class AddUser : PartialStateChange() {
+      object Loading : AddUser()
+      data class AddUserSuccess(val user: User) : AddUser()
+      data class AddUserFailure(val user: User, val throwable: Throwable) : AddUser()
+
+      override fun reduce(viewState: ViewState): ViewState {
+        return when (this) {
+          Loading -> viewState.copy(isLoading = true)
+          is AddUserSuccess -> viewState.copy(isLoading = false)
+          is AddUserFailure -> viewState.copy(isLoading = false)
+        }
+      }
+    }
+  }
+
+  sealed class SingleEvent {
+    data class AddUserSuccess(val user: User) : SingleEvent()
+    data class AddUserFailure(val user: User, val throwable: Throwable) : SingleEvent()
+  }
+}
